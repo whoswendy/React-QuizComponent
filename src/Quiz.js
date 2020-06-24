3f171c7c5b19542146d957a14cd404b9badaf5fa
Wendy Hu <wendy.hu@stonybrook.edu>
2020-06-24 14:48:27 -0400

    Completed QuizEnd

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js'
 
 let quizData = require('./quiz_data.json');
 
@@ -13,10 +14,14 @@ class Quiz extends Component {
   }
 
   render(){
-    return <div>
-      <QuizQuestion quiz_question=
-      {quizData.quiz_questions[this.state.quiz_position-1]}/>
-    </div>;
+    const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length;
+    return (
+        <div>
+          {isQuizEnd ? (<QuizEnd />) :
+          (<QuizQuestion quiz_question=
+          {quizData.quiz_questions[this.state.quiz_position-1]}/>)}
+        </div>
+    );
   }
 }
 
